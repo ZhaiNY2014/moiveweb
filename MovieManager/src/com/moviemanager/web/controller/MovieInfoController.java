245d2e9f4352661effa56d71899047596e473f32
ZhaiNY2014 <zny08@qq.com>
2017-05-16 22:51:19 +0800

    add select top10

--- MovieManager/src/com/moviemanager/web/controller/MovieInfoController.java
+++ MovieManager/src/com/moviemanager/web/controller/MovieInfoController.java
@@ -3,6 +3,9 @@ package com.moviemanager.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.moviemanager.web.jdbc.SpringjdbcConfig;
+
+import com.moviemanager.web.model.MovieModel;
 
 @Controller
 public class MovieInfoController {
@@ -10,7 +13,9 @@ public class MovieInfoController {
 	@RequestMapping(value = "/movieinfo")
 	public ModelAndView MovieInfo(){
 		ModelAndView mv = new ModelAndView();
-		
+		MovieModel movie = new MovieModel();
+		SpringjdbcConfig sjc = new SpringjdbcConfig();
+		movie = sjc.SelectMoiveName("123");
 		
 		return mv;
 	}
--- MovieManager/src/com/moviemanager/web/jdbc/SpringjdbcConfig.java
+++ MovieManager/src/com/moviemanager/web/jdbc/SpringjdbcConfig.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.moviemanager.web.model.MovieModel;
 	
@@ -37,7 +38,11 @@ public class SpringjdbcConfig {
 		datasource.setPassword("123456");
 		return datasource;
 	}*/
-	
+	/**
+	 * 这个函数用来查询单个的电影信息
+	 * @param moviename
+	 * @return MovieModel
+	 */
 	public MovieModel SelectMoiveName(String moviename){
 		String sql = "Select * from movieinfo where moviename=" + moviename ;
 		PreparedStatement pstm = null;
@@ -59,15 +64,49 @@ public class SpringjdbcConfig {
 			movie.setLanguage(rs.getString("language"));
 			movie.setShowdate(rs.getInt("showdate"));
 			movie.setScore(rs.getInt("score"));
-		}
-		
+		}	
 		}catch(SQLException ex){
 			System.out.println("SQLException:"+ex.getMessage());
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
-		
 		return movie;
+	}
+	
+	/**
+	 * 该函数用于top10的搜索
+	 * @return ArrayList<MovieModel>
+	 */
+	public ArrayList<MovieModel> SelectRank(){
+		String sql = "Select * from movieinfo where rank <= 10";
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		SpringjdbcConfig dbutil = new SpringjdbcConfig();
+		Connection con = null;
+		MovieModel movie = new MovieModel();
+		ArrayList<MovieModel> movielist = new ArrayList<>();
+		try{
+		con = dbutil.Getcon();
+		pstm = (PreparedStatement) con.prepareStatement(sql);
+		rs = pstm.executeQuery(sql);
+		
+		while(rs.next()){
+			movie.setMoviename(rs.getString("moviename"));
+			movie.setDirector(rs.getString("director"));
+			movie.setActor(rs.getString("actor"));
+			movie.setClassification(rs.getString("classification"));
+			movie.setCountry(rs.getString("country"));
+			movie.setLanguage(rs.getString("language"));
+			movie.setShowdate(rs.getInt("showdate"));
+			movie.setScore(rs.getInt("score"));
+			movielist.add(movie);
+		}	
+		}catch(SQLException ex){
+			System.out.println("SQLException:"+ex.getMessage());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return movielist;
 	}
 	
 }
